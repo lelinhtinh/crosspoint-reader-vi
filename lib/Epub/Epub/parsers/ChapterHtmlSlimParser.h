90db9cc52cb54cb0f607740387dae9972a580ca1
Lê Thành <lhthanh.pro@gmail.com>
2026-02-05 10:18:20 +0700

    refactor: resync with upstream

--- lib/Epub/Epub/parsers/ChapterHtmlSlimParser.h
+++ lib/Epub/Epub/parsers/ChapterHtmlSlimParser.h
@@ -18,7 +18,7 @@ class ChapterHtmlSlimParser {
   const std::string &filepath;
   GfxRenderer &renderer;
   std::function<void(std::unique_ptr<Page>)> completePageFn;
-  std::function<void(int)> progressFn; // Progress callback (0-100)
+  std::function<void()> popupFn; // Popup callback
   int depth = 0;
   int skipUntilDepth = INT_MAX;
   int boldUntilDepth = INT_MAX;
@@ -52,11 +52,11 @@ public:
                                  const uint8_t paragraphAlignment, const uint16_t viewportWidth,
                                  const uint16_t viewportHeight, const bool hyphenationEnabled,
                                  const std::function<void(std::unique_ptr<Page>)> &completePageFn,
-                                 const std::function<void(int)> &progressFn = nullptr)
+                                 const std::function<void()> &popupFn = nullptr)
       : filepath(filepath), renderer(renderer), fontId(fontId), lineCompression(lineCompression),
         extraParagraphSpacing(extraParagraphSpacing), paragraphAlignment(paragraphAlignment),
         viewportWidth(viewportWidth), viewportHeight(viewportHeight), hyphenationEnabled(hyphenationEnabled),
-        completePageFn(completePageFn), progressFn(progressFn) {}
+        completePageFn(completePageFn), popupFn(popupFn) {}
   ~ChapterHtmlSlimParser() = default;
   bool parseAndBuildPages();
   void addLineToPage(std::shared_ptr<TextBlock> line);
